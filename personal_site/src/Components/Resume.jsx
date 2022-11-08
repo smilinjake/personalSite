3d91f9deb7101348bafbc500e41d388a05092847
Jacob Ward <smilinjake@gmail.com>
2022-11-08 16:38:26 +0100

    deleting unused variable

--- personal_site/src/Components/Resume.jsx
+++ personal_site/src/Components/Resume.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
 import "../App.css";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
