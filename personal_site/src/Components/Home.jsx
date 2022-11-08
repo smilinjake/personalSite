10df6fbbe057ad403990f2a5800418be9b820801
Jacob Ward <smilinjake@gmail.com>
2022-11-08 16:07:59 +0100

    centered home page

--- personal_site/src/Components/Home.jsx
+++ personal_site/src/Components/Home.jsx
@@ -4,6 +4,7 @@ import "../App.css";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
 
 const ColorButton = styled(Button)(({ theme }) => ({
   // color: theme.palette.getContrastText('#fafafa'),
@@ -19,32 +20,34 @@ const ColorButton = styled(Button)(({ theme }) => ({
 }));
 const Home = () => {
   return (
-    <div className="Header">
-      <img
-        className="MyImage"
-        src="/images/websiteImage.jpeg"
-        alt="me"
-        height="250"
-        width="250"
-      ></img>
-      <Typography className="Tyopgraphy" variant="h5">
-        Web Developer
-      </Typography>
-      <Typography className="Tyopgraphy" variant="h1">
-        JAKE WARD
-      </Typography>
-      <nav className="links">
-        <ColorButton variant="outlined">
-          <Link className="Link" to="/contact">
-            Contact
-          </Link>
-        </ColorButton>
-        <ColorButton variant="outlined">
-          <Link className="Link" to="/info">
-            Info
-          </Link>
-        </ColorButton>
-      </nav>
+    <div className="HomePage">
+      <Grid className="Home" container item direction="column">
+        <img
+          className="MyImage"
+          src="/images/websiteImage.jpeg"
+          alt="me"
+          height="250"
+          width="250"
+        ></img>
+        <Typography className="Tyopgraphy" variant="h5">
+          Web Developer
+        </Typography>
+        <Typography className="Tyopgraphy" variant="h1">
+          JAKE WARD
+        </Typography>
+        <nav className="links">
+          <ColorButton variant="outlined">
+            <Link className="Link" to="/contact">
+              Contact
+            </Link>
+          </ColorButton>
+          <ColorButton variant="outlined">
+            <Link className="Link" to="/info">
+              Info
+            </Link>
+          </ColorButton>
+        </nav>
+      </Grid>
     </div>
   );
 };
